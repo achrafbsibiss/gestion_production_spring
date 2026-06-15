0f71495d2a88a37cd6d6117bcef3af42decb9091
achrafbsibiss <achraf.bsibiss11@gmail.com>
2026-06-16 00:08:57 +0100

    first commit

---
+++ src/main/java/org/example/gestion_product_spring/GestionProductSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestion_product_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProductSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionProductSpringApplication.class, args);
+    }
+
+}
